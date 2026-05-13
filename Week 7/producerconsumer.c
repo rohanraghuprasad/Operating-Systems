7db3927d594ac16916ace774c51b22d04d53b941
rohanraghuprasad <rohanraghu.cs24@bmsce.ac.in>
2026-05-13 12:37:25 +0530

    Add files via upload

---
+++ Week 7/producerconsumer.c
@@ -0,0 +1,79 @@
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#define BUFFER_SIZE 5
+
+sem_t empty, full, mutex;
+int buffer[BUFFER_SIZE];
+int in = 0, out = 0;
+
+void* producer(void* arg) {
+    int item = 1;
+
+    while (1) {
+        sem_wait(&empty);
+        sem_wait(&mutex);
+
+        buffer[in] = item;
+        printf("Produced: %d\n", item);
+        fflush(stdout);
+
+        item++;
+        in = (in + 1) % BUFFER_SIZE;
+
+        sem_post(&mutex);
+        sem_post(&full);
+
+        sleep(1);
+    }
+}
+
+void* consumer(void* arg) {
+    while (1) {
+        sem_wait(&full);
+        sem_wait(&mutex);
+
+        int item = buffer[out];
+        printf("Consumed: %d\n", item);
+        fflush(stdout);
+
+        out = (out + 1) % BUFFER_SIZE;
+
+        sem_post(&mutex);
+        sem_post(&empty);
+
+        sleep(1);
+    }
+}
+
+int main() {
+    pthread_t prod, cons;
+
+
+    sem_init(&empty, 0, BUFFER_SIZE);
+    sem_init(&full, 0, 0);
+    sem_init(&mutex, 0, 1);
+
+
+    if (pthread_create(&prod, NULL, producer, NULL) != 0) {
+        perror("Failed to create producer thread");
+        return 1;
+    }
+
+    if (pthread_create(&cons, NULL, consumer, NULL) != 0) {
+        perror("Failed to create consumer thread");
+        return 1;
+    }
+
+
+    pthread_join(prod, NULL);
+    pthread_join(cons, NULL);
+
+    sem_destroy(&empty);
+    sem_destroy(&full);
+    sem_destroy(&mutex);
+
+    return 0;
+}
